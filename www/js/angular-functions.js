e2d4c8d63f00c11652acbc9debabbb524d5a1249
Patrick Seelinger <patseelinger@gmail.com>
2017-08-11 20:42:21 -0400

    removing submodules

---
+++ www/js/angular-functions.js
@@ -0,0 +1,19 @@
+var app = angular.module('CNote', ['ngRoute']);
+
+app.controller('MainPageController', MainPageController);
+
+function MainPageController($scope){
+  $scope.all = true;
+  $scope.news = false;
+  $scope.sports = false;
+  $scope.events = false;
+
+  $scope.changeCategory = function(category){
+    $scope.all = false;
+    $scope.news = false;
+    $scope.sports = false;
+    $scope.events = false;
+
+    $scope[category] = true;
+  }
+}
---
+++ www/js/profile.js
@@ -0,0 +1,32 @@
+var user = firebase.auth().currentUser;
+var userId = user.uid;
+
+var passClicks = 0;
+var emailClicks = 0;
+
+  $('#profile-email').html(user.email);
+  $('#reset-email').on('click', function(){
+    if(emailClicks = 0){
+      $('#email-input').css('display', 'inline-block');
+      $('#reset-email').text("Submit");
+      emailClicks = 1;
+    }else{
+      var emailChange = $('#email-input').value;
+      writeProp('email', emailChange);
+      emailClicks = 0;
+    }
+  });
+
+$('#reset-password').on('click', function(){
+  if(passClicks = 0){
+  $('#password-input').css('display', 'inline-block');
+  }else{
+    var passChange = $('#password-input').value;
+  }
+});
+
+function writeProp(prop, change){
+  firebase.database().ref('users/' + userId).update({
+    prop: change,
+  });
+}
